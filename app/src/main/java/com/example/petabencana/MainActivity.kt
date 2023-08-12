34872ccdd4208f410ebeb24d1da9ad15b96ade5f
zamzamnurahman <kazamnn1912@gmail.com>
2023-08-12 23:11:40 +0700

    [update] -report layout- add card adapter with list item report

--- app/src/main/java/com/example/petabencana/MainActivity.kt
+++ app/src/main/java/com/example/petabencana/MainActivity.kt
@@ -1,38 +1,45 @@
 package com.example.petabencana
 
-import android.content.pm.PackageManager
-import android.content.res.Resources
-import android.location.Location
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.petabencana.adapter.ReportsAdapter
+import com.example.petabencana.api.ApiResponse
+import com.example.petabencana.api.Feature
+import com.example.petabencana.api.RetrofitClient
 import com.example.petabencana.databinding.ActivityMainBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
+    private val list = ArrayList<Feature>()
     private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val newsList = findViewById<RecyclerView>(R.id.news_list)
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(this)
+        RetrofitClient.instance.getReports(admin="ID-JB").enqueue(object : Callback<ApiResponse> {
+
+            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                response.body()?.let {
+                    list.addAll(it.result.features ?: emptyList())
+                }
+                val adapter = ReportsAdapter(list)
+                newsList.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
