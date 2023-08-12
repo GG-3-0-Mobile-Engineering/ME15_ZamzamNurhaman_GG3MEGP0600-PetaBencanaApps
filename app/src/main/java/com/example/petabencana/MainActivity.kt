6310e8774f09aaf8f91cdf4a51d1afbbd59b0f67
zamzamnurahman <kazamnn1912@gmail.com>
2023-08-13 00:03:57 +0700

    [update] -main feature- update maps feature

--- app/src/main/java/com/example/petabencana/MainActivity.kt
+++ app/src/main/java/com/example/petabencana/MainActivity.kt
@@ -10,15 +10,22 @@ import com.example.petabencana.api.ApiResponse
 import com.example.petabencana.api.Feature
 import com.example.petabencana.api.RetrofitClient
 import com.example.petabencana.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private val list = ArrayList<Feature>()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -26,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         val newsList = findViewById<RecyclerView>(R.id.news_list)
         newsList.setHasFixedSize(true)
         newsList.layoutManager = LinearLayoutManager(this)
-        RetrofitClient.instance.getReports(admin="ID-JB").enqueue(object : Callback<ApiResponse> {
+        RetrofitClient.instance.getReports(admin = "ID-JB").enqueue(object : Callback<ApiResponse> {
 
             override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
                 response.body()?.let {
@@ -40,6 +47,25 @@ class MainActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
 
+
         })
+
+        val mapsFragment = supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+
+        mapsFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(maps: GoogleMap) {
+        maps.clear()
+//        -6.901733255898841, 107.61896566917964
+        val latlang = LatLng(-6.901733255898841, 107.61896566917964)
+        maps.animateCamera(CameraUpdateFactory.newLatLngZoom(latlang, 8f))
+        val markerOpt = MarkerOptions()
+            .position(latlang)
+            .title("My Location")
+
+        maps.addMarker(markerOpt)
+
     }
 }
