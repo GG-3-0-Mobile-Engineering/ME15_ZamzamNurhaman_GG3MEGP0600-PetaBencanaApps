c3daf3fff49ffa3790b68ede74bb388bd3517301
zamzamnurahman <kazamnn1912@gmail.com>
2023-08-14 12:31:15 +0700

    [add] -navigation feature- add setting layout

--- app/src/main/java/com/example/petabencana/presentation/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/petabencana/presentation/ui/setting/SettingFragment.kt
@@ -6,19 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.petabencana.R
+import com.example.petabencana.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
 
 
     private lateinit var viewModel: SettingViewModel
+    private lateinit var _binding: FragmentSettingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        _binding = FragmentSettingBinding.inflate(inflater)
+        return _binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        _binding.appbarSetting.setNavigationOnClickListener {
+            _binding.appbarSetting.findNavController().navigateUp()
+        }
+    }
+
+
 
 
 
